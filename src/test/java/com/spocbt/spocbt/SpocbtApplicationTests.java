fb6760545e56d56f1b9bc9e17705b16170abd2fa
cbmja <cbmja@naver.com>
2025-01-17 00:41:59 +0900

    .

--- src/test/java/com/spocbt/spocbt/SpocbtApplicationTests.java
+++ src/test/java/com/spocbt/spocbt/SpocbtApplicationTests.java
@@ -1,12 +1,9 @@
 package com.spocbt.spocbt;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class SpocbtApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
