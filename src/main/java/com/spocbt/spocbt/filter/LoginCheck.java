fd923727cde62dffa142628c86f2adf7c91f972d
cbmja <cbmja@naver.com>
2025-01-22 01:08:14 +0900

    .

--- src/main/java/com/spocbt/spocbt/filter/LoginCheck.java
+++ src/main/java/com/spocbt/spocbt/filter/LoginCheck.java
@@ -2,6 +2,7 @@ package com.spocbt.spocbt.filter;
 
 
 import com.spocbt.spocbt.service.MemberService;
+import com.spocbt.spocbt.util.JvmCheck;
 import com.spocbt.spocbt.util.LoginUtil;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
@@ -13,6 +14,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -23,7 +25,10 @@ public class LoginCheck  implements Filter {
 
     private final LoginUtil loginUtil;
     private final MemberService memberService;
-    private static final Logger logger = LoggerFactory.getLogger(LoginCheck.class);
+
+    private final JvmCheck jvmCheck;
+    private final Logger logger = LoggerFactory.getLogger(LoginCheck.class);
+    private final ApplicationContext applicationContext;
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException {
@@ -35,6 +40,8 @@ public class LoginCheck  implements Filter {
 
 
         try{
+
+            jvmCheck.check(applicationContext);
 
             Cookie[] cookies = req.getCookies();
             String idCookie = "";
---
+++ src/main/java/com/spocbt/spocbt/util/JvmCheck.java
@@ -0,0 +1,55 @@
+package com.spocbt.spocbt.util;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryPoolMXBean;
+import java.lang.management.MemoryUsage;
+import java.util.Arrays;
+@Component
+@RequiredArgsConstructor
+public class JvmCheck {
+
+    private static final Logger logger = LoggerFactory.getLogger(JvmCheck.class);
+
+
+    public void check(ApplicationContext applicationContext){
+
+        ////////////////////////////////////////// jvm 메모리 체크용 코드 ////////////////////////////////////////////////////
+        logger.error("---------------------------------------- jvm check ----------------------------------------");
+        for (MemoryPoolMXBean memoryPoolMXBean : ManagementFactory.getMemoryPoolMXBeans()) {
+            // Metaspace 영역인지 확인
+            if ("Metaspace".equals(memoryPoolMXBean.getName())) {
+                MemoryUsage usage = memoryPoolMXBean.getUsage();
+                long used = usage.getUsed();
+                long max = usage.getMax();
+                long init = usage.getInit();
+
+                logger.error("Used: " + used / 1024 / 1024 + " MB");
+                logger.error("Max: " + (max != -1 ? max / 1024 / 1024 + " MB" : "Unlimited"));
+                logger.error("Init: " + init / 1024 / 1024 + " MB");
+            }
+        }
+        logger.error("---------------------------------------- jvm check ----------------------------------------");
+        String[] beanNames = applicationContext.getBeanDefinitionNames();
+        Arrays.sort(beanNames); // 알파벳 순 정렬
+        int i = 0;
+        for (String beanName : beanNames) {
+            i++;
+            //logger.error("Bean"+i+": " + beanName);
+        }
+        logger.error("---------------------------------------- bean cnt : "+i+" ----------------------------------------");
+
+        ////////////////////////////////////////// jvm 메모리 체크용 코드 ////////////////////////////////////////////////////
+
+
+    }
+
+
+
+
+}
